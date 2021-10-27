660d18697400778e3a55065f66f663f12287e523
cosmin bucur <cosmin.bucur@kambi.com>
2021-10-28 00:33:28 +0300

    rename main class

--- src/main/java/com/sda/project/PatentApp.java
+++ src/main/java/com/sda/project/PatentApp.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PatentApp {
 
-
     public static void main(String[] args) {
         SpringApplication.run(PatentApp.class, args);
     }
